78a0f70402c528346c5e7f847bb1881ca5a3e7f7
agniksaha <agniksaha9953@gmail.com>
2025-07-04 04:42:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
+
+function App() {
+  // "Egg", "Green Vegetable", "Mutton", "Fish", "Milk"
+  let foodItems = [];
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ErrorMessage items={foodItems}></ErrorMessage>
+      <FoodItems items={foodItems}></FoodItems>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Item.jsx
@@ -0,0 +1,6 @@
+const Item = (props) => {
+  let { foodItems } = props;
+  return <li className="list-group-item">{foodItems}</li>;
+};
+
+export default Item;
