9eaa5e08539be708efc7eed077378286a6fb26bd
agniksaha <agniksaha9953@gmail.com>
2025-08-17 05:49:52 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   // "Egg", "Green Vegetable", "Mutton", "Fish", "Milk"
-  let foodItems = [];
+  let foodItems = ["Egg", "Green Vegetable", "Mutton", "Fish", "Milk", "Roti"];
   return (
     <>
-      <h1>Healthy Food</h1>
-      <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodItems items={foodItems}></FoodItems>
+      <Container>
+        <h1 className="foodHeading">Healthy Food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>Above is the of healthy foods that your health and well being.</p>
+      </Container>
     </>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,12 @@
+import styles from "./Item.module.css";
+
 const Item = (props) => {
   let { foodItems } = props;
-  return <li className="list-group-item">{foodItems}</li>;
+  return (
+    <li className={`${styles["items"]} list-group-item items`}>
+      <span>{foodItems}</span>
+    </li>
+  );
 };
 
 export default Item;
